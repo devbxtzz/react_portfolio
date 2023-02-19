e9f73425462654156f024d5f411902463557b325
devbxtzz <batztonnie@gmail.com>
2023-02-19 20:44:04 +0300

    Updated Haeder

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
             <img src={profile} alt="Batya Tonny" />
 
             <div className='header__content'>
-                <h1>Batya Tonny</h1>
-                <p>Software Developer</p>
+                <h1>Tonny Batya</h1>
+                <p>Blockchain Developer</p>
                 <a href="mailto:batztonnie@gmail.com" className='button'>Get In Touch</a>
             </div>
         </section>
